5b9e65fb03d9a55634a61aa0130ec0d80d1ef75f
Paul Kohut <Paul Kohut>
2016-07-02 19:02:24 -0700

    Change versioning to geo_formulas_configure.h Update so, cmake -G "Visual Studio 14 2015", works. Update VS2015 projects Example of creating VS2015 project with CMake - cmake -DBoost_DEBUG=OFF -DBoost_USE_STATIC_LIBS=ON -DBoost_USE_MULTITHREADED=ON -DBOOST_LIBRARYDIR=c:\boost_1_61_0\lib32-msvc-14.0  -G "Visual Studio 14 2015" ..\GeoFormulas

---
+++ include/geo_formulas_configure.h
@@ -0,0 +1,8 @@
+#ifndef GEO_FORMULAS_CONFIGURE_H
+#define GEO_FORMULAS_CONFIGURE_H
+
+#define geo_formulas_VERSION_MAJOR 0
+#define geo_formulas_VERSION_MINOR 2
+#define geo_formulas_VERSION_PATCH 1
+
+#endif /* GEO_FORMULAS_CONFIGURE_H */
